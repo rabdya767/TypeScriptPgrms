056a6f53885d418619659102bbc4504203973053
Rabdya Santhosh <santhoshr.be22@uceou.edu>
2023-03-30 22:46:15 +0530

    remove dist folder files

--- dist/HB/genericTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* eslint-disable prefer-const */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-let boxString;
-let boxBoolean;
-let boxNumber;
-boxString = { contents: "Hello World!" };
-boxBoolean = { contents: true };
-boxNumber = { contents: 41 };
--- dist/casting.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const x = "Hello";
-console.log(x.length);
-// prints undefined since numbers don't have a length
-const y1 = "RABDYA SANTHOSH";
-console.log(`Length of the string ${y1} is ${y1.length}`);
